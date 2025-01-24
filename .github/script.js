d09d91c668cf2a2fd2997fb5e48b2ee4a75e91f9
baehl <baehl44@yahoo.fr>
2025-01-24 14:20:20 +0100

    fonction pour afficher rectangle OK

--- .github/script.js
+++ .github/script.js
@@ -1,8 +1,19 @@
 //Etape 1: Un triangle d’étoiles
-//Une ligne d’étoiles:
+//1.1 Une ligne d’étoiles:
 function afficherEtoiles(nbEtoiles){
     const etoile = '*'
-    console.log (`${etoile.repeat(nbEtoiles)}`)
+    return(`${etoile.repeat(nbEtoiles)}`)
 }
-afficherEtoiles(2)
-afficherEtoiles(5)
+//afficherEtoiles(2)
+//afficherEtoiles(5)
+
+//1.2 Carré d’étoiles
+function afficherRectangle(hauteur, largeur){
+    const rectangle = afficherEtoiles(largeur)
+    //console.log(rectangle)
+    for (let i=0; i< hauteur; i++){
+    console.log(rectangle)
+   }
+}
+
+afficherRectangle(5,5)
