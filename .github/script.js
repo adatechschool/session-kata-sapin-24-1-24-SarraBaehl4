0dfc06b2ef44a08a541561cbebf11de1147ee222
baehl <baehl44@yahoo.fr>
2025-01-24 15:46:17 +0100

    fonction triangleGauche OK

--- .github/script.js
+++ .github/script.js
@@ -9,9 +9,9 @@ function afficherEtoiles(nbEtoiles) {
 
 //1.2 Carré d’étoiles
 function afficherRectangle(hauteur, largeur) {
-  let rectangle ='';
-  for (let i =0; i< hauteur;i++){
-  rectangle += (afficherEtoiles(largeur) + "\n");
+  let rectangle = "";
+  for (let i = 0; i < hauteur; i++) {
+    rectangle += afficherEtoiles(largeur) + "\n";
   }
   return rectangle;
 }
@@ -19,12 +19,22 @@ function afficherRectangle(hauteur, largeur) {
 //console.log(afficherRectangle(3,4))
 
 //1.3 Triangle d’étoiles
-function afficherTriangleDroite(nbslash) {
-  let triangleDroite= ''
-  for (let i = 0; i < nbslash; i++) {
-    triangleDroite += (afficherEtoiles(i) + "\\" +'\n')
+function afficherTriangleDroite(nbSlash) {
+  let triangleDroite = "";
+  for (let i = 0; i < nbSlash; i++) {
+    triangleDroite += afficherEtoiles(i) + "\\" + "\n";
   }
-  return triangleDroite
-
+  return triangleDroite;
 }
-console.log(afficherTriangleDroite(5));
+//console.log(afficherTriangleDroite(5));
+
+//1.4 Seconde moitié du sapin et espaces: coté gauche du triangle
+function afficherTriangleGauche(nbSlash){
+  let triangleGauche="";
+  for (let i=0; i< nbSlash;i++){
+    const espaceAvantSlash = " ".repeat(nbSlash-i)
+    triangleGauche += (espaceAvantSlash + "/" +afficherEtoiles(i)+"\n")
+  }
+  return triangleGauche
+}
+console.log(afficherTriangleGauche(5))
