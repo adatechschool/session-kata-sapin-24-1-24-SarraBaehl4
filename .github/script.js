c47e4cdcfbec15583d2eb774269228d2996f1d02
baehl <baehl44@yahoo.fr>
2025-01-24 15:23:08 +0100

    fonction triangeDroite OK

--- .github/script.js
+++ .github/script.js
@@ -1,24 +1,30 @@
 //Etape 1: Un triangle d’étoiles
 //1.1 Une ligne d’étoiles:
-function afficherEtoiles(nbEtoiles){
-    const etoile = '*'
-    return(`${etoile.repeat(nbEtoiles)}`)
+function afficherEtoiles(nbEtoiles) {
+  const etoile = "*";
+  return `${etoile.repeat(nbEtoiles)}`;
 }
 //afficherEtoiles(2)
 //afficherEtoiles(5)
 
 //1.2 Carré d’étoiles
-function afficherRectangle(hauteur, largeur){
-    const rectangle = afficherEtoiles(largeur)+ '\n'
-    //console.log(rectangle)
-    return rectangle.repeat(hauteur)
-   
+function afficherRectangle(hauteur, largeur) {
+  let rectangle ='';
+  for (let i =0; i< hauteur;i++){
+  rectangle += (afficherEtoiles(largeur) + "\n");
+  }
+  return rectangle;
 }
 //console.log(afficherRectangle(5,5))
 //console.log(afficherRectangle(3,4))
 
 //1.3 Triangle d’étoiles
-function afficherTriangleDroite(nbslash){
-    console.log(afficherRectangle)
+function afficherTriangleDroite(nbslash) {
+  let triangleDroite= ''
+  for (let i = 0; i < nbslash; i++) {
+    triangleDroite += (afficherEtoiles(i) + "\\" +'\n')
+  }
+  return triangleDroite
 
 }
+console.log(afficherTriangleDroite(5));
