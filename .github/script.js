83f463c6b824319ee1806c3c0b6dc7b029c9778b
baehl <baehl44@yahoo.fr>
2025-01-24 13:54:04 +0100

    affichage d'une ligne d etoile OK

---
+++ .github/script.js
@@ -0,0 +1,8 @@
+//Etape 1: Un triangle d’étoiles
+//Une ligne d’étoiles:
+function afficherEtoiles(nbEtoiles){
+    const etoile = '*'
+    console.log (`${etoile.repeat(nbEtoiles)}`)
+}
+afficherEtoiles(2)
+afficherEtoiles(5)
